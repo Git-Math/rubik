87e1b1bca9cb46c5e26a6ac87e5f6398aac968db
Git-Math <mnguyen@student.42.fr>
2020-08-03 03:21:37 +0200

    rubik.py: add quiet option -q and remove some print

--- rubik.py
+++ rubik.py
@@ -4,20 +4,30 @@ import cube
 import solve
 
 def usage():
-    print("usage: %s <scramble>\n\
+    print("usage: %s [-q] <scramble>\n\
+-q: quiet, the cube is not printed\n\
 <scramble>: scramble to solve" % sys.argv[0])
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    quiet_mode = False
+    i_scramble = 1
+    if len(sys.argv) == 3:
+        if sys.argv[1] != "-q":
+            print("Invalid argument: %s" % sys.argv[1], file=sys.stderr)
+            usage()
+            sys.exit(1)
+        quiet_mode = True
+        i_scramble = 2
+    elif len(sys.argv) != 2:
         print("Invalid argument number", file=sys.stderr)
         usage()
         sys.exit(1)
-    elif len(sys.argv[1]) > 10000:
+    elif len(sys.argv[i_scramble]) > 10000:
         print("scramble too long", file=sys.stderr)
         usage()
         sys.exit(1)
 
-    scramble_move_list = sys.argv[1].split()
+    scramble_move_list = sys.argv[i_scramble].split()
     if len(scramble_move_list) == 0:
         print("<scramble> can't be empty", file=sys.stderr)
         usage()
@@ -25,21 +35,20 @@ if __name__ == '__main__':
 
     rubiks_cube = cube.Cube()
     rubiks_cube.move_sequence(scramble_move_list)
-    print("scramble: ", *scramble_move_list, sep = " ")
 
     if rubiks_cube.is_solved():
         print("This rubik's cube is already solved")
         sys.exit(0)
 
-    #ret = rubiks_cube.search_short_solution(cube.move_list, [], 3)
-    #if ret[0]:
-        #print("Short solution:", *ret[1])
-        #rubiks_cube.reverse_solution(ret[1])
-        #print_cube.print_cube(rubiks_cube, ret[1], None)
-        #sys.exit(0)
+    print(*scramble_move_list, sep = " ")
 
-    solution, step = solve.solve(rubiks_cube)
-    print("\nsolution: ", *solution, sep=" ")
-    print(f"solution move number: {len(solution)}")
-    rubiks_cube.reverse_solution(solution)
-    print_cube.print_cube(rubiks_cube, solution, step)
+    _, solution = rubiks_cube.search_short_solution(cube.move_list, [], 3)
+    step = None
+
+    if not solution:
+        solution, step = solve.solve(rubiks_cube)
+
+    print(*solution, sep=" ")
+    if not quiet_mode:
+        rubiks_cube.reverse_solution(solution)
+        print_cube.print_cube(rubiks_cube, solution, step)
